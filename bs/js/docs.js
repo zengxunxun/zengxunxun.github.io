49ae0e395f33a5c5872382b000e746008862acd1
Your Name <you@example.com>
2014-05-28 03:29:25 -0400

    Wed May 28 03:29:25 EDT 2014

---
+++ bs/js/docs.js
@@ -0,0 +1,22 @@
+/**
+ * @overview
+ *
+ * @author 
+ * @version 2013/11/26
+ */
+
+/*
+$(document).ready(function() {
+    var defaults = {
+        containerID: 'toTop', // fading element id
+        containerHoverID: 'toTopHover', // fading element hover id
+        scrollSpeed: 1200,
+        easingType: 'linear' 
+    };
+
+    $().UItoTop({ easingType: 'easeOutQuart' });
+
+    $('.fancybox').fancybox();
+});
+*/
+
